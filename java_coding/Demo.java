698f5c69eb6f00fde7754b7d457714d5088a97b1
ajithkumarkamla <ajithkumarkkamala@gmail.com>
2025-07-24 17:13:37 +0530

    java file

---
+++ java_coding/Demo.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=40;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name=sc.nextLine();
+        System.out.println("Enter your age:");
+        int age= sc.nextInt();
+
+        System.out.println("student name:"+name);
+
+        
+        
+        System.out.println("student age:"+age);
+       
+    }
+    
+}
