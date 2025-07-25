0b3b38f0a82e590918ac39af6b0dfc7af98b156b
ajithkumarkamla <ajithkumarkkamala@gmail.com>
2025-07-25 18:01:12 +0530

    update file

---
+++ java_coding/Operators.java
@@ -0,0 +1,25 @@
+class Operators{
+
+    public static void main(String[] args)
+    {
+        // int a=10;
+        // System.out.println("a:"+a);
+        // a +=30;// a =a+30
+        //   System.out.println("add:"+a);
+        // // a -=20;
+        //  a =a-20;
+      
+        //   System.out.println("sub:"+a);
+        compersion_Oper();
+
+    }
+    public static void compersion_Oper()
+    {
+        int num1=50;
+        int num2=50;
+     System.out.println("compate two value:"+(num1==num2));
+      System.out.println("not eqult:"+(num1!=num2));
+      System.out.println("grater than are e:"+(num1>=num2));
+    }
+
+}
