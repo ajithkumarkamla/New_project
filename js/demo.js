85a2bdde69b18458fd2f1f6f11bddd6db9d33b3d
ajithkumarkamla <ajithkumarkkamala@gmail.com>
2025-07-25 16:13:52 +0530

    update file

--- js/demo.js
+++ js/demo.js
@@ -1,7 +1,58 @@
-let  a=10;
-let b=40;
+// let  a=10;
+// let b=40;
 
-let total=a+b;
-console.log("additoin:",total);
-let name1="karthika";
-console.log("name:",name1S)
+// let total=a+b;
+// console.log("additoin:",total);
+// let name1="karthika";
+// console.log("name:",name1S)
+
+// var x=50;
+// console.log("x value:",x);
+// x=150;
+// console.log("reassing value:",x);
+// var x=501;
+// console.log(x);
+// console.log("-------------")
+
+// let  a=49
+// console.log(a)
+// a="vignesh";
+// console.log(a)
+// a="40";
+// console.log(a)
+
+
+//datatypes
+
+// let name="palanivelu";
+// let course='javascript';
+// let city=`perambalur`;
+// let age=21;
+// let bool=true
+// console.log("name",name);
+// console.log("course:",course);
+// console.log("city:",city);
+// console.log("type:",typeof(name))
+// console.log("age type:",typeof(age))
+// console.log("bool",typeof(bool))
+
+// let gender;
+// console.log(gender)
+// console.log("gender:",typeof(gender))
+// let fees=null;
+// console.log("type fees:",typeof(fees))
+// 
+// let num1=10
+// let num2=2
+// console.log("addition:",(num1+num2));
+// console.log("MDOELUS:",(num1%num2));
+// console.log("division:",(num1/num2))
+// console.log("Exponentiation (Power):",(num1**num2))
+// console.log(++num1);
+// console.log(num1++);
+// console.log(num1)
+
+let num1=10
+num1 +=30;// num1=num1+40
+num1 %=3
+console.log("assinment operators:",num1)
