fa7b9a9ffef9709b87fdac3fb1ff67d9a1f4fb66
ajithkumarkamla <ajithkumarkkamala@gmail.com>
2025-07-26 16:40:08 +0530

    using

--- js/demo.js
+++ js/demo.js
@@ -52,7 +52,78 @@
 // console.log(num1++);
 // console.log(num1)
 
-let num1=10
-num1 +=30;// num1=num1+40
-num1 %=3
-console.log("assinment operators:",num1)
+// let num1=10
+// num1 +=30;// num1=num1+40
+// num1 %=3
+// console.log("assinment operators:",num1)
+
+// let a= 20
+// let b=1
+// console.log(a==b)//not check datatype
+// console.log(a===b); //check datatype and values
+// console.log(a<b);
+// console.log(a<=b);
+// console.log(a>b);
+// console.log(a>=b)
+// console.log(a!=b)
+
+// let a=9
+// let b=10
+// let c=15
+// let d=3
+// console.log("And:",(a>b)&&(c<d))
+// console.log("or:",(a>b)||(c<d))
+// console.log("not ",!((a>b)&&(c<d)))
+
+// // let a=7
+// // let b=8
+// // console.log(a&b)
+// // console.log(a|b)
+// console.log(11|5)
+// console.log(11&5)
+
+
+// variables type == 1. let , 2. var,3. const
+
+// var a=10 
+// console.log(a)
+// a=40
+// console.log(a)
+// var a=30;
+// console.log(a)
+
+// //let 
+// console.log("----------------")
+// let c=40
+// console.log(c)
+// c=50
+// console.log(c)
+// let d=49
+// console.log(c)
+// // const
+// const name="karthika";
+// console.log(name)
+// const course="javascript"
+
+//javascripts datatypes
+
+// let age=20
+
+// let name="karthika"
+// let course='full stack python'
+// let  gender=`female`
+// let per=80.60
+// let result=true
+// let fees;
+// let  email=null
+// var id=Symbol()
+
+
+// console.log("age:",age)
+// console.log("age datatype:",typeof(age))
+// console.log(typeof(course))
+// console.log(typeof(per))
+// console.log(typeof(result))
+// console.log(typeof(fees))
+// console.log(typeof(null))
+// console.log(typeof(id))
